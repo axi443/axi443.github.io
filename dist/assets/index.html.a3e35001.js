e130ce0c0def01824e78dcb824948bdbc1a141ca
wangzihao <1315926581@qq.com>
2022-10-17 13:57:33 +0800

    remove dist

--- dist/assets/index.html.a3e35001.js
+++
@@ -1,1 +0,0 @@
-const t={key:"v-d5d13fc0",path:"/tags/test/",title:"Tags",lang:"en-US",frontmatter:{title:"Tags",layout:"Tags"},excerpt:"",headers:[],git:{},filePathRelative:null};export{t as data};
--- dist/assets/index.html.f6485b37.js
+++
@@ -1,1 +0,0 @@
-const t={key:"v-1645d86b",path:"/tags/front%20end/",title:"Tags",lang:"en-US",frontmatter:{title:"Tags",layout:"Tags"},excerpt:"",headers:[],git:{},filePathRelative:null};export{t as data};
